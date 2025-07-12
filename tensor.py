1d741fa5c869d568a0d769288d5fa0a2c9e0bb85
sungpil-kim <foxstork@gmail.com>
2025-07-12 22:24:17 +0900

    Update tensor.py

--- tensor.py
+++ tensor.py
@@ -6,13 +6,13 @@ import tensorflow as tf
 텐서 = tf.constant([3,4,5])
 텐서2 = tf.constant([6,7,8])
 텐서3 = tf.zeros([3,2,3])
-텐서4 = tf.zeros([1,2,3],
-                 [4,5,6])
+텐서4 = tf.constant([[1,2,3],
+                 [4,5,6]])
 
 print(텐서+텐서2) #[9,11,13]
 
 print(텐서3) #[[[0,0,0]]] [[[0,0,0]]] [[0,0,0]]]
 
-print(텐서3.shape) #(1,3)
+print(텐서2.shape) #(1,3)
 
 print(텐서4.shape) #(2,3)
