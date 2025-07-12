4e7b479189a931e2e4d505cd93239440bb3c9325
sungpil-kim <foxstork@gmail.com>
2025-07-12 22:29:18 +0900

    Update tensor.py

--- tensor.py
+++ tensor.py
@@ -3,16 +3,14 @@
 ## 텐서플로어 내용
 import tensorflow as tf
 
-텐서 = tf.constant([3,4,5])
+텐서 = tf.constant([3,4,5]) # 1차원 구조
 텐서2 = tf.constant([6,7,8])
 텐서3 = tf.zeros([3,2,3])
 텐서4 = tf.constant([[1,2,3],
-                 [4,5,6]])
+                 [4,5,6]])    # 2차원 구조(행렬)
 
 print(텐서+텐서2) #[9,11,13]
 
 print(텐서3) #[[[0,0,0]]] [[[0,0,0]]] [[0,0,0]]]
-
-print(텐서2.shape) #(1,3)
 
 print(텐서4.shape) #(2,3)
